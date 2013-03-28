55f1fd8a24ae3dd9a93e9f9749fbfca8cc42ac78
Emmet O'Grady <emmet@studentcentral.ie>
2013-03-28 23:25:53 +0100

    Moved the render() method to the WebController, added documentation to TwigView

--- src/Emmetog/Controller/WebController.php
+++ src/Emmetog/Controller/WebController.php
@@ -4,33 +4,61 @@ namespace Emmetog\Controller;
 
 use Emmetog\Controller\Controller;
 use Emmetog\Router\Route;
+use Emmetog\View\ViewInterface;
+use Emmetog\Cache\CacheInterface;
 
 abstract class WebController extends Controller
 {
 
     /**
-     * @var \Apl\View\ViewInterface
+     * The view object.
+     * 
+     * @var ViewInterface
      */
     protected $view;
 
     /**
-     * @var \Apl\Cache\CacheInterface
+     * The cache object.
+     * 
+     * @var CacheInterface
      */
     protected $cache;
-    protected $templateRoot = '';
+    
+    /**
+     * The variables to pass to the view.
+     *
+     * @var array
+     */
     protected $viewVariables = array();
+    
+    /**
+     * The layout template to use in the view.
+     *
+     * @var string
+     */
     protected $layout = '';
 
+    /**
+     * Creates a new instance of a controller.
+     * 
+     * @param Config $config The config object.
+     */
     public function __construct($config)
     {
         parent::__construct($config);
     }
 
+    /**
+     * Executes the controller.
+     */
     final public function execute()
     {
         $this->cache = $this->config->getCache();
 
         $cacheDefinition = $this->cacheDefinition();
+        /**
+         * @todo Process and implement the cacheDefinition
+         */
         $assignedVars = array();
 
 //        if ($cacheDefinition)
@@ -64,34 +92,66 @@ abstract class WebController extends Controller
                 $this->cache->set(__CLASS__, serialize($this->viewVariables));
             }
         }
-        
-        if (!$this->layout)
-        {
-            $this->layout = $route->getController() . DIRECTORY_SEPARATOR
-                    . $route->getAction() . '.tpl';
-        }
-        
+
         $this->render($this->viewVariables, $this->layout);
     }
 
+    /**
+     * Assigns a variable to the view.
+     * 
+     * @param string $variable The name of the variable (its name in the view).
+     * @param mixed $value The value of the variable.
+     */
     public function assign($variable, $value)
     {
         $this->viewVariables[$variable] = $value;
     }
 
+    /**
+     * Gets the definition of the cache.
+     */
     abstract protected function cacheDefinition();
-    
-    abstract protected function render($viewVars, $template);
 
+    /**
+     * Renders the view.
+     * 
+     * @param array $variables The variables to pass to the view.
+     * @param string $template The template to render.
+     */
+    protected function render($variables, $template)
+    {
+        $this->view = new \Apl\View\TwigView($this->config);
+
+        foreach ($variables as $variableName => $variableValue)
+        {
+            $this->view->assign($variableName, $variableValue);
+        }
+
+        $this->view->setTemplate($template);
+
+        $this->view->render();
+    }
+
+    /**
+     * Executes before the build, can be overwritten by children.
+     */
     public function preBuild()
     {
         
     }
-    
-    public function setLayout($layout) {
+
+    /**
+     * Sets the layout template.
+     */
+    public function setLayout($layout)
+    {
         $this->layout = $layout;
     }
 
+}
+
+class WebControllerException extends \Exception {
+    
 }
 
 ?>
--- src/Emmetog/View/TwigView.php
+++ src/Emmetog/View/TwigView.php
@@ -7,19 +7,46 @@ use Emmetog\View\View;
 class TwigView extends View
 {
 
+    /**
+     * The variables that are assigned to this view.
+     * 
+     * @var array
+     */
     protected $assignedVars = array();
+    
+    /**
+     * The template (layout) to use to display the view.
+     *
+     * @var string
+     */
     protected $template = '';
 
+    /**
+     * Assign a variable to this view.
+     * 
+     * @param string $variable The name of the variable to assign.
+     * @param mixed $value The value of the variable.
+     */
     public function assign($variable, $value)
     {
         $this->assignedVars[$variable] = $value;
     }
 
+    /**
+     * Gets all the variables that are assigned to this view.
+     * 
+     * @return array
+     */
     public function getAssignedVariables()
     {
         return $this->assignedVars;
     }
 
+    /**
+     * Unassigns a variable from this view.
+     * 
+     * @param string $variable The variable name to unassign.
+     */
     public function removeAssignedVariable($variable)
     {
         if (array_key_exists($variable, $this->assignedVars))
@@ -28,29 +55,50 @@ class TwigView extends View
         }
     }
 
+    /**
+     * Sets the template for this view.
+     * 
+     * @param string $templatePath The path to the template (inside the Template dir).
+     */
     public function setTemplate($templatePath)
     {
         $this->template = $templatePath;
     }
 
+    /**
+     * Renders the view.
+     * 
+     * @throws TwigViewNoTemplateSpecifiedException If the template was not specified.
+     */
     public function render()
     {
-        // Not needed now that we are using composer.
-//        require_once 'Twig/Autoloader.php';
-//        \Twig_Autoloader::register();
-        
         // Get the template directory.
         $template_directory = APP_ROOT_DIRECTORY . 'Template' . DIRECTORY_SEPARATOR;
         $loader = new \Twig_Loader_Filesystem($template_directory);
         $options = array();
         $twig = new \Twig_Environment($loader, $options);
         
+        if (empty($this->template))
+        {
+            throw new TwigViewNoTemplateSpecifiedException();
+        }
+
         $template = $twig->loadTemplate($this->template);
         $html = $template->render($this->assignedVars);
-        
+
         echo $html;
     }
 
+}
+
+class TwigViewException extends \Exception
+{
+    
+}
+
+class TwigViewNoTemplateSpecifiedException extends TwigViewException
+{
+    
 }
 
 ?>
